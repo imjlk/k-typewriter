4bda860c4744bc8274575bc7e6d0ac508e946c2f
imjlk <i@m.junglei.kim>
2024-06-18 01:46:38 +0900

    First commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6968f730725fb43fb4c0');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '998a7ff9edec813dae91');
