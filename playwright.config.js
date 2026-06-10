22a666513ee9ff23c3ed4bddf1547f8240d38bda
imjlk <i@m.junglei.kim>
2026-06-11 08:08:29 +0900

    Use system Chrome for GitHub smoke tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require( '@playwright/test' );
 
 const baseURL = process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:8888';
+const browserChannel = process.env.PLAYWRIGHT_BROWSER_CHANNEL || undefined;
 
 module.exports = defineConfig( {
 	testDir: './tests/smoke',
@@ -19,6 +20,7 @@ module.exports = defineConfig( {
 	},
 	use: {
 		baseURL,
+		channel: browserChannel,
 		screenshot: 'only-on-failure',
 		trace: 'retain-on-failure',
 	},
